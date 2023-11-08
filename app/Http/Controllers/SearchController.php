621356b8dd57de0f0869d72a3865be97f7f5701f
Jinit <jinitvyas0107@gmail.com>
2023-11-08 23:13:29 +0530

    SearchFeatureWorking

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,11 +10,8 @@ class SearchController extends Controller
 {
     function getSearchResults(Request $r){
         $SearchText = $_GET['searchText'];
-        $ThreadResultSet = Thread::where('ttext','like','%'.$SearchText.'%')->join('users', 'uid', '=', 'users.uid')->get();
-        $UsersResultSet = Users::where('uname','like','%'.$SearchText.'%')
-                            ->orWhere('fname','like','%'.$SearchText.'%')
-                            ->orWhere('lname','like','%'.$SearchText.'%')
-                            ->get();
+        $ThreadResultSet = Thread::where('ttext','like','%'.$SearchText.'%')->get();
+        $UsersResultSet = Users::where('uname','like','%'.$SearchText.'%')->get();
         return view('SearchResult', [ 'SearchText' => $SearchText , 'ThreadResultSet' => $ThreadResultSet , 'UsersResultSet' => $UsersResultSet ] );
     }
 }
--- resources/views/SearchResult.blade.php
+++ resources/views/SearchResult.blade.php
@@ -1,39 +1,35 @@
 @extends('layout/main')
 
 @section('main-content')
+@php
+    use App\Models\Users;
+@endphp
     <div class="main h-100 pt flex-center flex-column text-light">
-
         <div class="card-group bg-dark text-light flex-center flex-column w-50 rounded-capsule">
-            @php
-                foreach ($UsersResultSet as $user) {                    
-                    @endphp
-                    <div class="card text-start bg-dark w-100">
-                        <div class="card-body w-100 d-flex">
-                            <a href="/Profile/{{$user->uid}}" class="text-light d-flex">
-                                <i class="bi bi-person h3 me-3"></i>
-                                <h4>@</h4><h4 class="card-title">{{$user->uname}}</h4>
-                            </a>
-                        </div>
+            @foreach ($UsersResultSet as $user)    
+                <div class="card text-start bg-dark w-100">
+                    <div class="card-body w-100 d-flex">
+                        <a href="/Profile/{{$user->uid}}" class="text-light d-flex">
+                            <i class="bi bi-person h3 me-3"></i>
+                            <h4>@</h4><h4 class="card-title"> {{$user->uname}}</h4>
+                        </a>
                     </div>
-                    @php
-                }
+                </div>
+            @endforeach
+        </div>
+        <div class="card-group bg-transparent text-light flex-center flex-column w-75 rounded">
+            @foreach ($ThreadResultSet as $Thread)
+                @php
+                    $uname = Users::where('uid',$Thread->uid)->first();
                 @endphp
-        </div>
-        <div class="card-group bg-dark text-light flex-center flex-column w-50 rounded-capsule">
-            @php
-                foreach ($ThreadResultSet as $Thread) {
-                    echo $Thread;
-                    @endphp
-                        <div class='card bg-dark my-3 mx-2 border-light border-start-0 w-100' id='threadList'>
-                            <h4 class='card-title'>@". $uname ."</h4>
-                            <div class='card-body'>
-                            <p class='card-text'>". $Thread->ttext ."</p>
-                        </div>
-                    @php
-                }
-            @endphp
-        </div>
-            @php
-        @endphp
+                <div class='card bg-dark my-3 mx-2 border-light border-start-0 w-50' id='threadList'>
+                    <a href="/Profile/{{$uname->uid}}" class="text-light d-flex h4">
+                        @<h4 class='card-title' >{{$uname->uname}}</h4>
+                    </a>
+                    <div class='card-body'>
+                        <p class='card-text'> {{$Thread->ttext}} </p>
+                    </div>
+                </div>
+            @endforeach
     </div>
 @endsection
